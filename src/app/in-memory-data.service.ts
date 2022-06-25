e1756b567d5ff8e9d1d53f68a2304c06b55c2b4b
Hung Tran <hungtran891989@gmail.com>
2022-06-25 10:14:16 +0700

    fix hero updates

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -23,6 +23,8 @@ export class InMemoryDataService implements InMemoryDbService {
   }
 
   genId(heroes: Hero[]): number {
-    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) : 11;
+    return heroes.length > 0
+      ? Math.max(...heroes.map(hero => hero.id)) + 1
+      : 11;
   }
 }
